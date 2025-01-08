67c50f79fae903b5447d3000f8fb8a65cb696889
Maxheinrich2024 <heinrichmax13@gmail.com>
2025-01-08 14:08:01 +0100

    valider et modifier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ const TITLE = `Cyclopolis - Suivi des ${config.revName.plural} par ${config.asso
 const DESCRIPTION =
   `Plateforme citoyenne et associative, par ${config.assoName}. État d'avancement, cartes interactives des itinéraires, détails, travaux : suivez le développement du réseau cyclable sécurisé lyonnais`;
 const BASE_URL = 'https://cyclopolis.fr';
-const COVER_IMAGE_URL = 'https://cyclopolis.lavilleavelo.org/cyclopolis.png';
+const COVER_IMAGE_URL = '/INSERT LOGO MARANA GOLO.png';
 
 export default defineNuxtConfig({
   app: {
