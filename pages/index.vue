e77b806082872c1527dd6e8a7aeb032f8c4f3137
Maxheinrich2024 <heinrichmax13@gmail.com>
2025-01-09 17:22:38 +0100

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -17,19 +17,7 @@
       <Stats :voies="voies" class="mt-8" />
       <Typology :voies="voies" class="mt-8 max-w-2xl mx-auto" />
     </div>
-    <div class="max-w-7xl mx-auto mt-14 px-4 sm:px-6 lg:px-8 lg:mt-24">
-      <div class="space-y-8 sm:space-y-12">
-        <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl text-center">
-          <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
-            Avancement par ligne
-          </h2>
-          <p class="text-xl text-gray-500">
-            Choisissez une {{ getRevName('singular') }} pour connaitre le détail du projet et voir son niveau d'avancement.
-          </p>
-        </div>
-        <HomeLinesSection class="mt-5" />
-      </div>
-    </div>
+  
     <div class="py-16">
       <LvvCta />
     </div>
