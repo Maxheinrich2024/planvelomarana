e216867636b824c67f3f67ef33a78c8ce9cf554f
Maxheinrich2024 <heinrichmax13@gmail.com>
2025-01-09 14:29:20 +0100

    ok

--- pages/plan-officiel/index.vue
+++ pages/plan-officiel/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full">
     <img
-      src="public/PLAN OFFICIEL.png"
+      src='public/PLAN OFFICIEL.png'
       class="object-contain w-full h-screen"
       :alt="`plan officiel des ${getRevName()}`"
     >
